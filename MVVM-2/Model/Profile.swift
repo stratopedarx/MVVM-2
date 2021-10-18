a420806f945c7a33900a8a4fa06dad441777381d
Sergey Lobanov <sergey.lobanov@orioninc.com>
2021-10-18 17:15:17 +0300

    Start project with MVC architecture

---
+++ MVVM-2/Model/Profile.swift
@@ -0,0 +1,15 @@
+//
+//  Profile.swift
+//  MVVM-2
+//
+//  Created by Sergey Lobanov on 18.10.2021.
+//
+
+import Foundation
+
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
---
+++ MVVM-2/View/TableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.swift
+//  MVVM-2
+//
+//  Created by Sergey Lobanov on 18.10.2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+}
