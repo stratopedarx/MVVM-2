2d1381d4ab35b34e655a8bcc22b6e82619979ee7
Sergey Lobanov <sergey.lobanov@orioninc.com>
2021-10-18 16:27:27 +0300

    Initial Commit

---
+++ MVVM-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-2
+//
+//  Created by Sergey Lobanov on 18.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
